1478ebde3d3d037a35e2f41b8d517e72d6907409
dandeboer <ddeboer926@gmail.com>
2020-10-07 19:51:13 -0400

    polished up nav ui some, got the basic structure for home page, got description to change periodically

---
+++ index.js
@@ -0,0 +1,33 @@
+class Animations {
+    constructor() {
+        this.time = 0
+        this.description = document.querySelector('.home-description')
+        this.listOfDescriptions = ['am a hard worker', 'learn quickly', 'want you to check out my LinkedIn', 'am a kind and friendly person', 'work well in a team', 'want to do a good job', 'am a creative problem solver']
+        this.usedDescriptions = ['love cats', 'think analytically', 'have 11 flanel shirts']
+    }
+    async homeDescription() {
+        console.log(this.description.innerText)
+        await new Promise(r => setTimeout(r, 3000));
+        if (this.time == 3) {
+            this.description.innerText = "am a Full Stack Web Developer"
+            this.time = 0
+            console.log(this.time)
+            this.homeDescription()
+        }
+        else {
+            let randomDescription = Math.floor(Math.random() * this.listOfDescriptions.length)
+            this.description.innerText = this.listOfDescriptions[randomDescription]
+            this.usedDescriptions.push(this.listOfDescriptions[randomDescription])
+            this.listOfDescriptions.splice(randomDescription, 1, this.usedDescriptions[0])
+            this.usedDescriptions.shift()
+            this.time++
+            console.log(this.time)
+            this.homeDescription()
+        }
+    }
+}
+
+new Animations().homeDescription()
+
+
+// make it so that the part of the website you are on is underlined in the nav bar while you are in it
