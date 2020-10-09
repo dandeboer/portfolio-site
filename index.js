f80e54c732cee95cc0aa22ac356a7dbe5856bff9
dandeboer <ddeboer926@gmail.com>
2020-10-09 01:49:49 -0400

    the description will now cycle automatically with javascript rather than hover. refactored some css to be more javascript friendly

--- index.js
+++ index.js
@@ -1,33 +1,75 @@
 class Animations {
     constructor() {
         this.time = 0
-        this.description = document.querySelector('.home-description')
+        this.homeDescription1 = document.querySelector('#home-description-1')
+        this.homeDescription2 = document.querySelector('#home-description-2')
+        this.descriptionShown = 1
         this.listOfDescriptions = ['am a hard worker', 'learn quickly', 'want you to check out my LinkedIn', 'am a kind and friendly person', 'work well in a team', 'love to do a good job', 'am a creative problem solver']
         this.usedDescriptions = ['love cats', 'think analytically', 'have 11 flanel shirts']
     }
-    async homeDescription() {
-        await new Promise(r => setTimeout(r, 3000));
+    async homeDescriptionText() {
+        await new Promise(r => setTimeout(r, 3000))
+        this.homeDescriptionAnimation()
         if (this.time == 3) {
-            this.description.innerText = "am a Full Stack Web Developer"
+            if (this.descriptionShown == 1) {
+                this.homeDescription2.innerText = "am a Full Stack Web Developer"
+                this.descriptionShown = 2
+            }
+            else {
+                this.homeDescription1.innerText = "am a Full Stack Web Developer"
+                this.descriptionShown = 1
+            }
             this.time = 0
-            this.homeDescription()
+            this.homeDescriptionText()
         }
         else {
             let randomDescription = Math.floor(Math.random() * this.listOfDescriptions.length)
-            this.description.innerText = this.listOfDescriptions[randomDescription]
+            if (this.descriptionShown == 1) {
+                this.homeDescription2.innerText = this.listOfDescriptions[randomDescription]
+                this.descriptionShown = 2
+            }
+            else {
+                this.homeDescription1.innerText = this.listOfDescriptions[randomDescription]
+                this.descriptionShown = 1
+            }
             this.usedDescriptions.push(this.listOfDescriptions[randomDescription])
             this.listOfDescriptions.splice(randomDescription, 1, this.usedDescriptions[0])
             this.usedDescriptions.shift()
             this.time++
-            this.homeDescription()
+            this.homeDescriptionText()
         }
     }
-    descriptionAnimation() {
-
+    async homeDescriptionAnimation() {
+        if (this.descriptionShown == 1) {
+            this.homeDescription1.classList.toggle('width0')
+            this.homeDescription1.classList.toggle('opacity0')
+            this.homeDescription1.classList.toggle('left100')
+            this.homeDescription2.classList.toggle('width0')
+            this.homeDescription2.classList.toggle('opacity0')
+            await new Promise(r => setTimeout(r, 500))
+            this.homeDescription1.classList.toggle('display-none')
+            this.homeDescription1.classList.toggle('description-transition')
+            this.homeDescription1.classList.toggle('left100')
+            this.homeDescription1.classList.toggle('display-none')
+            this.homeDescription1.classList.toggle('description-transition')
+        }
+        else {
+            this.homeDescription2.classList.toggle('width0')
+            this.homeDescription2.classList.toggle('opacity0')
+            this.homeDescription2.classList.toggle('left100')
+            this.homeDescription1.classList.toggle('width0')
+            this.homeDescription1.classList.toggle('opacity0')
+            await new Promise(r => setTimeout(r, 500))
+            this.homeDescription2.classList.toggle('display-none')
+            this.homeDescription2.classList.toggle('description-transition')
+            this.homeDescription2.classList.toggle('left100')
+            this.homeDescription2.classList.toggle('display-none')
+            this.homeDescription2.classList.toggle('description-transition')
+        }
     }
 }
 
-new Animations().homeDescription()
+new Animations().homeDescriptionText()
 
 
 // make it so that the part of the website you are on is underlined in the nav bar while you are in it
