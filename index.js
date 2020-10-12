9848994c06397200c7c1349e98391db9eaac13b4
dandeboer <ddeboer926@gmail.com>
2020-10-12 19:38:24 -0400

    the nav buttons will now scroll to their section. the buttons will be dynamically underlined when in their corresponding section

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 class Animations {
     constructor() {
-        this.time = 0
+        this.descriptionTime = 0
         this.homeDescription1 = document.querySelector('#home-description-1')
         this.homeDescription2 = document.querySelector('#home-description-2')
         this.descriptionShown = 1
         this.listOfDescriptions = ['am a hard worker', 'want you to check out my LinkedIn', 'am a kind and friendly person',
             'love to do a good job', 'am a creative problem solver', 'like conscructive criticism']
         this.usedDescriptions = ['love cats', 'learn quickly', 'work well in a team']
+        this.homeButton = document.querySelector('#home-button')
+        this.aboutButton = document.querySelector('#about-button')
+        this.projectsButton = document.querySelector('#projects-button')
+        this.contactButton = document.querySelector('#contact-button')
+        this.homeContainer = document.querySelector('#home-container')
+        this.navTicking = false
+        this.sectionHeight = this.homeContainer.offsetHeight
+        this.currentSection
+        this.yTracking
     }
+
+    setup() {
+        this.homeDescriptionText()
+        window.addEventListener('scroll', this.navUnderlineTicker.bind(this))
+        window.addEventListener('resize', this.updateSectionHeight.bind(this))
+        this.navUnderlineTicker()
+        this.homeButton.addEventListener('click', this.homeScroll.bind(this))
+        this.aboutButton.addEventListener('click', this.aboutScroll.bind(this))
+        this.projectsButton.addEventListener('click', this.projectsScroll.bind(this))
+        this.contactButton.addEventListener('click', this.contactScroll.bind(this))
+    }
+
     async homeDescriptionText() {
         await new Promise(r => setTimeout(r, 3500))
         this.homeDescriptionAnimation()
-        if (this.time == 3) {
+        if (this.descriptionTime == 3) {
             if (this.descriptionShown == 1) {
                 this.homeDescription2.innerText = 'am a Full Stack Web Developer'
                 this.descriptionShown = 2
@@ -20,7 +41,7 @@ class Animations {
                 this.homeDescription1.innerText = 'am a Full Stack Web Developer'
                 this.descriptionShown = 1
             }
-            this.time = 0
+            this.descriptionTime = 0
             this.homeDescriptionText()
         }
         else {
@@ -36,10 +57,11 @@ class Animations {
             this.usedDescriptions.push(this.listOfDescriptions[randomDescription])
             this.listOfDescriptions.splice(randomDescription, 1, this.usedDescriptions[0])
             this.usedDescriptions.shift()
-            this.time++
+            this.descriptionTime++
             this.homeDescriptionText()
         }
     }
+
     async homeDescriptionAnimation() {
         if (this.descriptionShown == 1) {
             this.homeDescription1.classList.toggle('description-fade')
@@ -56,78 +78,65 @@ class Animations {
             this.homeDescription2.classList.toggle('left100')
         }
     }
-}
 
-class Functionality {
-    constructor() {
-        this.homeButton = document.querySelector('#home-button')
-        this.aboutButton = document.querySelector('#about-button')
-        this.projectsButton = document.querySelector('#projects-button')
-        this.contactButton = document.querySelector('#contact-button')
+    navUnderlineTicker() {
+        if (!this.navTicking) {
+            window.requestAnimationFrame(this.navUnderlineAnimation.bind(this))
+            this.navTicking = true
+        }
     }
-    setup() {
-        // bind() is used to keep the correct scope for the this keyword. Otherwise the
-        // value for this will become the element the eventlistener is firing on
-        this.homeButton.addEventListener('click', this.homeScroll.bind(this))
+
+    navUnderlineAnimation() {
+        this.yTracking = this.homeContainer.getBoundingClientRect().y * -1 + 65
+        if (this.yTracking < (this.sectionHeight / 2) && this.currentSection != 'home') {
+            this.currentSection = 'home'
+            this.navRemoveUnderline()
+            this.homeButton.classList.add('nav-underline')
+        }
+        else if ((this.sectionHeight / 2) < this.yTracking && this.yTracking < ((this.sectionHeight / 2) + this.sectionHeight) && this.currentSection != 'about') {
+            this.currentSection = 'about'
+            this.navRemoveUnderline()
+            this.aboutButton.classList.add('nav-underline')
+        }
+        else if (((this.sectionHeight / 2) + this.sectionHeight) < this.yTracking && this.yTracking < ((this.sectionHeight / 2) + (2 * this.sectionHeight)) && this.currentSection != 'projects') {
+            this.currentSection = 'projects'
+            this.navRemoveUnderline()
+            this.projectsButton.classList.add('nav-underline')
+        }
+        else if (((this.sectionHeight / 2) + (2 * this.sectionHeight)) < this.yTracking && this.currentSection != 'contact') {
+            this.currentSection = 'contact'
+            this.navRemoveUnderline()
+            this.contactButton.classList.add('nav-underline')
+        }
+        this.navTicking = false
     }
-    removeUnderline() {
+
+    navRemoveUnderline() {
         this.homeButton.classList.remove('nav-underline')
+        this.aboutButton.classList.remove('nav-underline')
+        this.projectsButton.classList.remove('nav-underline')
+        this.contactButton.classList.remove('nav-underline')
     }
+
+    updateSectionHeight() {
+        this.sectionHeight = this.homeContainer.offsetHeight
+    }
+
     homeScroll() {
         window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
-        this.homeButton.classList.toggle('nav-underline')
     }
-    // aboutScroll() {
 
-    // }
-    // projectsScroll() {
+    aboutScroll() {
+        window.scrollTo({top: this.sectionHeight, left: 0, behavior: 'smooth'})
+    }
 
-    // }
-    // contactScroll() {
+    projectsScroll() {
+        window.scrollTo({top: (2 * this.sectionHeight), left: 0, behavior: 'smooth'})
+    }
 
-    // }
-}
-
-new Animations().homeDescriptionText()
-new Functionality().setup()
-
-
-
-let ticking = false
-let divHeight = window.innerHeight - 65
-let home = document.querySelector('#home-container')
-let currentSection
-let hButton = document.querySelector('#home-button')
-let aButton = document.querySelector('#about-button')
-
-
-console.log(divHeight)
-window.addEventListener('scroll', test)
-
-function removeUnderline() {
-    hButton.classList.remove('nav-underline')
-    aButton.classList.remove('nav-underline')
-}
-
-function test() {
-    if (!ticking) {
-        window.requestAnimationFrame(function() {
-            yTracking = home.getBoundingClientRect().y * -1 + 65
-            console.log(yTracking)
-            if (yTracking < (divHeight / 2) && currentSection != 'home') {
-                currentSection = 'home'
-                removeUnderline()
-                hButton.classList.add('nav-underline')
-            }
-            else if (yTracking > (divHeight / 2) && currentSection != 'about') {
-                currentSection = 'about'
-                removeUnderline()
-                aButton.classList.add('nav-underline')
-            }
-            ticking = false
-        })
-        ticking = true
+    contactScroll() {
+        window.scrollTo({top: (3 * this.sectionHeight), left: 0, behavior: 'smooth'})
     }
 }
 
-test()
+new Animations().setup()
